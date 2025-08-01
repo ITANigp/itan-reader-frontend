4055fc42d3964564308f006e0595214d53a45f54
Oluyemi Paul <oluola96@gmail.com>
2025-08-01 10:08:09 +0100

    Update ReadersFooter.js

--- app/reader/(components)/ReadersFooter.js
+++ app/reader/(components)/ReadersFooter.js
@@ -26,7 +26,7 @@ const Footer = ({ hiddenPage }) => {
     pathname.startsWith("/author") || pathname.startsWith("/dashboard");
 
   return (
-    <footer className={`${hiddenPage} bg-[#000000] w-full hidden`}>
+    <footer className={`bg-[#000000] w-full hidden`}>
       <section className="bg-[#000000] w-full max-w-7xl mx-auto px-4 pt-8 pb-0 flex flex-col gap-8">
         <div className="flex flex-col sm:flex-row sm:justify-between sm:items-start gap-8">
           {/* Logo & Socials */}
