4634d76afa28e43bf7840b99b93a793cd89bc1bf
Ayokunnumi1 <omololuayk@gmail.com>
2026-03-31 20:37:20 +0100

    Create blog page function

--- app/(authorLandingPage)/TheGroitReview/page.jsx
+++ app/(authorLandingPage)/TheGroitReview/page.jsx
@@ -1,7 +1,7 @@
-import BlogClient from './BlogClient'
-import { client } from '@/lib/sanity'
+import BlogClient from "./BlogClient";
+import { client } from "@/lib/sanity";
 
-export const dynamic = 'force-dynamic' // <- this forces SSR
+export const dynamic = "force-dynamic"; // <- this forces SSR
 
 export default async function BlogPage() {
   const query = `*[_type == "post" && defined(slug.current)] | order(_createdAt desc){
@@ -19,12 +19,12 @@ export default async function BlogPage() {
       title,
       description
     }
-  }`
+  }`;
 
-  const posts = await client.fetch(query)
+  const posts = await client.fetch(query);
 
   return (
-    <div className='pt-6 min-h-screen bg-gray-100'>
+    <div className="pt-6 min-h-screen bg-gray-100">
       <BlogClient posts={posts} />
     </div>
   );
